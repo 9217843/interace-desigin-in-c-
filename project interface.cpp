6eaf8518da2addbea34e0e671d3bf8e206827309
9217843 <87167047+9217843@users.noreply.github.com>
2021-07-08 23:22:42 -0700

    Add files via upload
    HY my name is ahmad i  create this programm lets check and rate it it my own logic

---
+++ project interface.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+using namespace std;
+
+void start()
+{
+int time=300;
+
+int pause=400;
+
+	
+
+	
+	
+
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+
+	cout<<"\t\t\t\t\t  _ _ _ _ _ _ _ _ _ _ _ _ _   "<<endl;
+cout<<"\t\t\t\t\t |                         |"<<endl;
+system("color F");
+	cout<<"\t\t\t\t\t | S";
+	
+	Sleep(time);
+	cout<<"C";
+	Sleep(time);
+	cout<<"H";
+	Sleep(time);
+cout<<"O";
+	Sleep(time);
+	cout<<"O";
+	Sleep(time);
+	cout<<"L";
+	Sleep(pause);
+	
+	system ("color B");
+	cout<<" M";
+	Sleep(time);
+	cout<<"A";
+	Sleep(time);
+	cout<<"N";
+	Sleep(time);
+	cout<<"G";
+	Sleep(time);
+	cout<<"M";
+	Sleep(time);
+	cout<<"E";
+	Sleep(time);
+cout<<"N";
+	Sleep(time);
+	cout<<"T";
+	system ("color E");
+	Sleep(pause);	
+	cout<<" S";
+	Sleep(time);
+	cout<<"Y";
+	Sleep(time);
+	cout<<"S";
+	Sleep(time);
+	cout<<"T";
+	Sleep(time);
+	cout<<"E";
+	Sleep(time);
+	cout<<"M  |"<<endl;	
+	Sleep(pause);
+cout<<"\t\t\t\t\t |_ _ _ _ _ _ _ _ _ _ _ _ _| "<<endl;
+Sleep(pause);
+cout<<"\t\t\t\t\t |_ _ _ _ _ _ _ _ _ _ _ _ _| "<<endl;
+system ("color A");
+}
+
+
+int main()
+{   
+	start();	
+}
